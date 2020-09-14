a08facdc6a5371888eaf13ee5c26999daf087a88
Wagner Nascimento <wagneer_nascimento@live.com>
2020-09-14 02:43:49 -0300

    alinhando textView , usando biblioteca de terceiros

--- app/src/main/java/app/equipe41/projetoong/Models/Voluntario.kt
+++ app/src/main/java/app/equipe41/projetoong/Models/Voluntario.kt
@@ -1,11 +1,11 @@
 package app.equipe41.projetoong.Models
 
 data class Voluntario (
+    val _id: String,
     val nome_voluntario: String,
     val telefone_voluntario: String,
-    val cnpj_ong: String,
+    val cpf_voluntario: String,
     val email: String,
-    val senha: String,
     val endereco: String,
     val numero: Int,
     val id_ong: String
